f8dba497ab1cc4c7042f1b87800708d3a432a6de
samuferreiram <141786578+samuferreiram@users.noreply.github.com>
2023-11-30 18:19:29 -0300

    .

---
+++ desafio-felipao/index.js
@@ -0,0 +1,27 @@
+let nomeHeroi = "Calabreso"
+let xpHeroi = 3300
+
+if(xpHeroi<1000){
+    console.log("O Herói" + nomeHeroi + "está no nível Ferro.")
+}
+else if((xpHeroi<1000) && (xpHeroi<=2000)){
+    console.log("O Herói" + nomeHeroi + "está no nível Bronze.")
+}
+else if((xpHeroi<2000) && (xpHeroi<=5000)){
+    console.log("O Herói" + nomeHeroi + "está no nível Prata.")
+}
+else if((xpHeroi<5000) && (xpHeroi<=7000)){
+    console.log("O Herói" + nomeHeroi + "está no nível Ouro.")
+}
+else if((xpHeroi<7000) && (xpHeroi<=8000)){
+    console.log("O Herói" + nomeHeroi + "está no nível Platina.")
+}
+else if((xpHeroi<8000) && (xpHeroi<=9000)){
+    console.log("O Herói" + nomeHeroi + "está no nível Ascendente.")
+}
+else if((xpHeroi<9000) && (xpHeroi<=10000)){
+    console.log("O Herói" + nomeHeroi + "está no nível Imortal.")
+}
+else if(xpHeroi<10000){
+    console.log("O Herói" + nomeHeroi + "está no nível Radiante.")
+}
