3d5dd553cfc7e750bc7e459b2a78a3b048fbc44c
samuferreiram <141786578+samuferreiram@users.noreply.github.com>
2023-11-30 18:39:35 -0300

    .

--- desafio-felipao/index.js
+++ desafio-felipao/index.js
@@ -1,27 +1,32 @@
-let nomeHeroi = "Calabreso"
-let xpHeroi = 3300
+let heroi = ["Calabreso", 11000]
 
-if(xpHeroi<1000){
-    console.log("O Herói" + nomeHeroi + "está no nível Ferro.")
+function dadosHeroi(){
+    const xpHeroi = heroi[1]
+
+    if(xpHeroi<1000){
+        return "Ferro"
+    }
+    else if((xpHeroi>1000 && xpHeroi<=2000)){
+        return "Bronze"
+    }
+    else if((xpHeroi>2000 && xpHeroi<=5000)){
+        return "Prata"
+    }
+    else if((xpHeroi>5000 && xpHeroi<=7000)){
+        return "Ouro"
+    }
+    else if((xpHeroi>7000 && xpHeroi<=8000)){
+        return "Platina"
+    }
+    else if((xpHeroi>8000 && xpHeroi<=9000)){
+        return "Ascendente"
+    }
+    else if((xpHeroi>9000 && xpHeroi<=10000)){
+        return "Imortal"
+    }
+    else{
+        return "Radiante"
+    }
 }
-else if((xpHeroi<1000) && (xpHeroi<=2000)){
-    console.log("O Herói" + nomeHeroi + "está no nível Bronze.")
-}
-else if((xpHeroi<2000) && (xpHeroi<=5000)){
-    console.log("O Herói" + nomeHeroi + "está no nível Prata.")
-}
-else if((xpHeroi<5000) && (xpHeroi<=7000)){
-    console.log("O Herói" + nomeHeroi + "está no nível Ouro.")
-}
-else if((xpHeroi<7000) && (xpHeroi<=8000)){
-    console.log("O Herói" + nomeHeroi + "está no nível Platina.")
-}
-else if((xpHeroi<8000) && (xpHeroi<=9000)){
-    console.log("O Herói" + nomeHeroi + "está no nível Ascendente.")
-}
-else if((xpHeroi<9000) && (xpHeroi<=10000)){
-    console.log("O Herói" + nomeHeroi + "está no nível Imortal.")
-}
-else if(xpHeroi<10000){
-    console.log("O Herói" + nomeHeroi + "está no nível Radiante.")
-}
+
+console.log("O Herói " + heroi[0] + " está no nível " + dadosHeroi())
